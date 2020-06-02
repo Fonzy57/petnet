c91939b60bf1354fb239767abaf33ed5ca4e6d65
Fonzy57 <traben@hotmail.fr>
2020-06-02 15:05:39 +0200

    mise en place des différentes pages du projet

---
+++ src/Controller/PetnetController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PetnetController extends AbstractController
+{
+    /**
+     * @Route("/petnet", name="petnet")
+     */
+    public function index()
+    {
+        return $this->render('petnet/index.html.twig', [
+            'controller_name' => 'PetnetController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home() {
+        return $this->render('petnet/home.html.twig', [
+            'title' => 'Petnet',
+        ]);
+    }
+
+    /**
+     * @Route("/informations", name="informations")
+     */
+    public function informations(){
+        return $this->render('petnet/informations.html.twig', [
+            'title' => 'Informations',
+        ]);
+    }
+
+}
